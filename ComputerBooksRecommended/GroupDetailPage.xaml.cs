c211ae803af574554f46c92fba346ec76aa27956
yonney <mikiyonney@gmail.com>
2014-05-27 19:10:08 +0800

    push sourceCode

---
+++ ComputerBooksRecommended/GroupDetailPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using ComputerBooksRecommended.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “组详细信息页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234229 上提供
+
+namespace ComputerBooksRecommended
+{
+    /// <summary>
+    /// 显示单个组的概述的页，包括组内各项
+    /// 的预览。
+    /// </summary>
+    public sealed partial class GroupDetailPage : ComputerBooksRecommended.Common.LayoutAwarePage
+    {
+        public GroupDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 使用在导航过程中传递的内容填充页。在从以前的会话
+        /// 重新创建页时，也会提供任何已保存状态。
+        /// </summary>
+        /// <param name="navigationParameter">最初请求此页时传递给
+        /// <see cref="Frame.Navigate(Type, Object)"/> 的参数值。
+        /// </param>
+        /// <param name="pageState">此页在以前会话期间保留的状态
+        /// 字典。首次访问页面时为 null。</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: 创建适用于问题域的合适数据模型以替换示例数据
+            var group = SampleDataSource.GetGroup((String)navigationParameter);
+            this.DefaultViewModel["Group"] = group;
+            this.DefaultViewModel["Items"] = group.Items;
+        }
+
+        /// <summary>
+        /// 在单击某个项时进行调用。
+        /// </summary>
+        /// <param name="sender">显示所单击项的 GridView (在应用程序处于对齐状态时
+        /// 为 ListView)。</param>
+        /// <param name="e">描述所单击项的事件数据。</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 导航至相应的目标页，并
+            // 通过将所需信息作为导航参数传入来配置新页
+            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+    }
+}
---
+++ ComputerBooksRecommended/ItemDetailPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ComputerBooksRecommended.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “项详细信息页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234232 上提供
+
+namespace ComputerBooksRecommended
+{
+    /// <summary>
+    /// 显示组内单个项的详细信息同时允许使用手势
+    /// 浏览同一组的其他项的页。
+    /// </summary>
+    public sealed partial class ItemDetailPage : ComputerBooksRecommended.Common.LayoutAwarePage
+    {
+        public ItemDetailPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// 使用在导航过程中传递的内容填充页。在从以前的会话
+        /// 重新创建页时，也会提供任何已保存状态。
+        /// </summary>
+        /// <param name="navigationParameter">最初请求此页时传递给
+        /// <see cref="Frame.Navigate(Type, Object)"/> 的参数值。
+        /// </param>
+        /// <param name="pageState">此页在以前会话期间保留的状态
+        /// 字典。首次访问页面时为 null。</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // 允许已保存页状态重写要显示的初始项
+            if (pageState != null && pageState.ContainsKey("SelectedItem"))
+            {
+                navigationParameter = pageState["SelectedItem"];
+            }
+
+            // TODO: 创建适用于问题域的合适数据模型以替换示例数据
+            var item = SampleDataSource.GetItem((String)navigationParameter);
+            this.DefaultViewModel["Group"] = item.Group;
+            this.DefaultViewModel["Items"] = item.Group.Items;
+            this.flipView.SelectedItem = item;
+        }
+
+        /// <summary>
+        /// 保留与此页关联的状态，以防挂起应用程序或
+        /// 从导航缓存中放弃此页。值必须符合
+        /// <see cref="SuspensionManager.SessionState"/> 的序列化要求。
+        /// </summary>
+        /// <param name="pageState">要使用可序列化状态填充的空字典。</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+            var selectedItem = (SampleDataItem)this.flipView.SelectedItem;
+            pageState["SelectedItem"] = selectedItem.UniqueId;
+        }
+    }
+}
